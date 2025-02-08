472b09711a8ee2054c4d46613e738df558fb271c
Joseph Mwanzia <makamwanzia@gmail.com>
2025-02-08 23:32:12 +0300

    feat: add the main entry file

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import express from 'express'
 import 'dotenv/config'
 const { PORT } = process.env
 const app = express()
+import {router} from './routes/routes.js'
 
-app.get('/', (req, res) => {
-    res.send('Hello There!')
-})
+app.use(express.json())
+app.use('/',router)
 
 app.listen(PORT, () => {
     console.log(`App listening @ http://localhost:${PORT}`)
