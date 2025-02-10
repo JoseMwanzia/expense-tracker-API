512149c64a5de5b088bbe4bb7fb9fc56fb457e47
Joseph Mwanzia <makamwanzia@gmail.com>
2025-02-10 23:59:44 +0300

    chore: add expense model

---
+++ model/expenseModel.js
@@ -0,0 +1,12 @@
+import sql from "../config/dbConfig"
+
+export default class Expense {
+    static async getAllExpenses( user_id, limit, offset ) {
+        try {
+            const data = await sql `SELECT * FROM todo_list WHERE user_id=${user_id} ORDER BY id DESC LIMIT ${limit} OFFSET ${offset};`
+            return data
+        } catch (error) {
+            throw new Error(`Error querying data from the database, ${error}`)
+        }
+    }
+}
