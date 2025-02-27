81ae1a27687b7d7a5eb4bb14469ac09f17645bac
Joseph Mwanzia <makamwanzia@gmail.com>
2025-02-27 23:55:38 +0300

    fix: add the login component

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 import express from 'express'
 const router = express.Router()
 import { signUp }  from '../controllers/usersControllers.js'
+import { login } from '../controllers/authServer.js'
 
 router.post('/signup', signUp)
 router.post('/login', login)
