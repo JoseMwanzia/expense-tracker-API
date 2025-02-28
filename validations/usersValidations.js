0f79c31dd301a30ae1df3b6a729c54ff5e83e72c
Joseph Mwanzia <makamwanzia@gmail.com>
2025-02-28 23:26:51 +0300

    chore: add userValidation object

---
+++ validations/usersValidations.js
@@ -0,0 +1,7 @@
+import Joi from "joi";
+
+export const signupSchema = Joi.object({
+    name: Joi.string().required().alphanum().min(2).max(30),
+    email: Joi.string().email().required(),
+    password: Joi.string().required()
+})
