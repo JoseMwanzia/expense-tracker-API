2b2874b733f6d2eb684eb25809f334f37f11bd18
Joseph Mwanzia <makamwanzia@gmail.com>
2025-03-01 23:58:02 +0300

    fix: loginSchema added

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ import express from 'express'
 const router = express.Router()
 import { signUp }  from '../controllers/usersControllers.js'
 import { login } from '../controllers/authServer.js'
-import { signupSchema } from '../validations/usersValidations.js'
+import { signupSchema, loginSchema } from '../validations/usersValidations.js'
 import { validationMiddleware } from '../middlewares/validationMiddleware.js'
 
 router.post('/signup', validationMiddleware(signupSchema), signUp)
