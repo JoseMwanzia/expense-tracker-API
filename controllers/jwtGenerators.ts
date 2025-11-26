424df5df45ce29180aabcb8ee6bf43398a26675f
Joseph Mwanzia <makamwanzia@gmail.com>
2025-11-27 02:19:25 +0300

    refactor: use typescript

--- controllers/jwtGenerators.ts
+++ controllers/jwtGenerators.ts
@@ -1,13 +1,22 @@
 import jwt from 'jsonwebtoken'
 import { redis } from '../config/redisConfig'
+import 'dotenv/config'
 const { ACCESS_TOKEN, REFRESH_TOKEN, ACCESS_TOKEN_EXPIRES, REFRESH_TOKEN_EXPIRES } = process.env
 
-export async function generateAccessToken(data: Object) {
-    return jwt.sign(data, ACCESS_TOKEN!, {expiresIn: Number(ACCESS_TOKEN_EXPIRES)})
+
+
+export function generateAccessToken(data: Object) {
+    if (!ACCESS_TOKEN) {
+        throw new Error("ACCESS_TOKEN is not defined in environment variables");
+    }
+    return jwt.sign(data, ACCESS_TOKEN, { expiresIn: ACCESS_TOKEN_EXPIRES as any });
 }
 
-export async function generateRefreshToken(data: Object) {
-    const refreshToken = jwt.sign(data, REFRESH_TOKEN!, {expiresIn: Number(REFRESH_TOKEN_EXPIRES)})
+export async function generateRefreshToken(data: object) {
+    if (!REFRESH_TOKEN) {
+        throw new Error("REFRESH_TOKEN is not defined in environment variables");
+    }
+    const refreshToken = jwt.sign(data, REFRESH_TOKEN, {expiresIn: REFRESH_TOKEN_EXPIRES as any})
 
     //✅ store refreshToken in Redis with an expiry, for referencing.
     const storeRefreshToken = await redis.set(refreshToken, JSON.stringify(data), 'EX', 7 * 24 * 60 * 60)
